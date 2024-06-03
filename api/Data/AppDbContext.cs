26c49c1070e424d1807baf171ade4e1eb529bd9f
Jose E Rodriguez-Marrero <jose.rodriguez-marrero@proton.me>
2024-06-03 02:26:23 -0700

    Add Identity boilerplate code

--- api/Data/AppDbContext.cs
+++ api/Data/AppDbContext.cs
@@ -1,14 +1,16 @@
 using api.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace api.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<AppUser>
     {
         public AppDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
         { }
 
         public DbSet<Stock> Stocks { get; set; }
         public DbSet<Comment> Comments { get; set; }
+        public DbSet<AppUser> Users { get; set; }
     }
 }
