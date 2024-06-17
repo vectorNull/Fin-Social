780e75ba4ee289c5d149bcac19d60a3fa332fc5c
Jose E Rodriguez-Marrero <jose.rodriguez-marrero@proton.me>
2024-06-17 12:34:07 -0700

    Build login endpoint; modify swagger to use token

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using api.Interfaces;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
@@ -13,11 +14,13 @@ namespace api.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly ITokenService _tokenService;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
             _tokenService = tokenService;
+            _signInManager = signInManager;
         }
 
         [HttpPost("register")]
@@ -48,9 +51,9 @@ namespace api.Controllers
                         return Ok(
                             new NewUserDto
                             {
-                            UserName = appuser.UserName,
-                            Email = appuser.Email,
-                            Token = _tokenService.CreateToken(appuser)
+                                UserName = appuser.UserName,
+                                Email = appuser.Email,
+                                Token = _tokenService.CreateToken(appuser)
                             }
                         );
                     }
@@ -68,6 +71,44 @@ namespace api.Controllers
             {
                 return StatusCode(500, ex);
             }
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+        
+            var user = await _userManager.Users.FirstOrDefaultAsync(x =>
+                x.UserName == loginDto.UserName.ToLower());
+        
+            if (user is null)
+            {
+                return Unauthorized("Invalid Username");
+            }
+        
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+        
+            if (!result.Succeeded)
+            {
+                return Unauthorized("Invalid Password and/or Username");
+            }
+        
+            if (user is not null)
+            {
+                return Ok(
+                    new NewUserDto
+                    {
+                        UserName = user.UserName,
+                        Email = user.Email,
+                        Token = _tokenService.CreateToken(user)
+                    }
+                );
+            }
+        
+            return StatusCode(500, "Unexpected error occurred.");
         }
     }
 }
